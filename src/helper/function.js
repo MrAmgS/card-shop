1102ab7dea0e974e77a9c0d890eee4dec3426884
MrAmgS <a.h.gharaei@hotmail.com>
2023-07-22 16:00:35 +0330

    add some helper function & add icons to assets

--- src/helper/function.js
+++ src/helper/function.js
@@ -3,6 +3,20 @@ const shorten = (title) => {
     const newTitle = `${splitedTitle[0]} ${splitedTitle[1]}`
 
     return newTitle
-}
+} // to short product name
 
-export {shorten};
+const isInCart = (state , id) => {
+    const result = !!state.selectedItems.find(item => item.id === id);
+    return result;
+} //to check an Item is in card 
+
+const quantityCount = (state , id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id);
+    if( index === -1){
+        return false
+    } else {
+        return state.selectedItems[index].quantity;
+    }
+} // to get quantity of an Item
+
+export {shorten , isInCart , quantityCount};
