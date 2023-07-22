5d23b3aba182e16c18a22fb7920ed54b7b8de9da
MrAmgS <a.h.gharaei@hotmail.com>
2023-07-22 16:31:07 +0330

    buttons added to products Component

--- src/shared/Products.js
+++ src/shared/Products.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+//helper
+import { shorten , quantityCount ,isInCart } from '../helper/function';
+
+//Context
+import { CartContext } from '../context/CartContextProvider';
+
+//Icons
+import deleteIcon from '../assets/icons/trash.svg'
 
 const Products = ({productData}) => {
-
+    const {state , dispatch} = useContext(CartContext);
+    
     return (
-        <>
+       <div>
+        <img src={productData.image} alt='product' />
+        <h3>{shorten(productData.title)}</h3>
+        <p>{productData.price} $</p>
         <div>
-            <img src={productData.image} alt='product' />
+            <Link to='/products' >Details</Link>
+            <div>
+                {(quantityCount(state , productData.id) === 1) && <button onClick={() => dispatch({type :"REMOVE_ITEM" , payload : productData})} > <img src={deleteIcon} alt='delete' /></button> }
+                {(quantityCount(state , productData.id) > 1) && <button onClick={() => dispatch ({type: "DECREASE" , payload : productData})}  > - </button> }
+                {
+                    quantityCount (state , productData.id > 0 ) && <span >{quantityCount(state , productData.id)}</span>
+                }
+               {
+                 isInCart(state , productData.id) ? 
+                    <button onClick={() =>dispatch({type : "INCREASE" , payload : productData})}> + </button> :
+                    <button onClick={() =>dispatch({type : "ADD_ITEM" , payload : productData})}>Add to Cart</button>
+                }
+            </div>
         </div>
-            
-        </>
+       </div>
     );
 };
 
