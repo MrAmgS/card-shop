4bb6b98b42ac105923db7a38696211ae2db25ab5
MrAmgS <a.h.gharaei@hotmail.com>
2023-07-23 16:29:44 +0330

    product details page styled

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
+//styles 
+import styles from './ProductDetail.module.css'
+
 const ProductDetail = () => {
     const params = useParams()
     const id = params.id;
@@ -20,16 +23,16 @@ const ProductDetail = () => {
     const { image, title, category, description, price } = data;
 
     return (
-        <div>
-           <img src={image} alt='product' />
-           <div>
+        <div className={styles.container}>
+           <img className={styles.image} src={image} alt='product' />
+           <div className={styles.textContainer}>
             <h2>{title}</h2>
-            <p>{description}</p>
-            <p>
-                <span>Category : </span> {category} 
+            <p className={styles.description}>{description}</p>
+            <p className={styles.category}>
+                <span > Category : </span> {category} 
             </p>
-            <div>
-                <span>{price}</span>
+            <div className={styles.buttonContainer} >
+                <span className={styles.price}>{price} $</span>
                 <Link to='/products'>Back to shop</Link>
             </div>
            </div>
