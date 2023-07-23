62fe7b9e9495dcae4911a520cf0f540a328822f3
MrAmgS <a.h.gharaei@hotmail.com>
2023-07-23 17:46:38 +0330

    ShopCart and Cart completed and get styles

--- src/Components/ShopCart.js
+++ src/Components/ShopCart.js
@@ -3,46 +3,49 @@ import { Link } from 'react-router-dom';
 
 
 //Components
+import Cart from './shared/Cart';
 
 
 //Context
 import { CartContext } from '../context/CartContextProvider';
-import Cart from './shared/Cart';
+
+//styles
+import styles from './ShopCart.module.css';
 
 const ShopCart = () => {
     const {state ,dispatch} = useContext(CartContext);
 
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.cartContainer}>
                 {state.selectedItems.map( (item) => (
                 <Cart key={item.id} data={item}  />))}
             </div>
             {state.itemsCounter > 0 && (
-        <div >
+        <div className={styles.payments} >
           <p>
             <span>Total items :</span> 
           </p>
           <p>
             <span>Total Payments : </span> {state.total}
           </p>
-          <div >
-            <button onClick={() => dispatch({ type: "CHECKOUT" })}>
+          <div className={styles.buttonContainer} >
+            <button className={styles.checkout} onClick={() => dispatch({ type: "CHECKOUT" })}>
               Checkout
             </button>
-            <button  onClick={() => dispatch({ type: "CLEAR" })}>Clear</button>
+            <button className={styles.clear} onClick={() => dispatch({ type: "CLEAR" })}>Clear</button>
           </div>
         </div>
       )}
       {state.checkout && (
-        <div >
+        <div className={styles.complete} >
           <h3> checkout Successfully</h3>
           <Link to="/products"> Buy More</Link>
         </div>
       )}
 
       { !state.checkout && state.itemsCounter === 0 && (
-        <div >
+        <div className={styles.complete}>
           <h3> Clear Successfully , Want To Buy?</h3>
           <Link to="/products"> Go To Shop</Link>
         </div>
--- src/Components/shared/Cart.js
+++ src/Components/shared/Cart.js
@@ -3,23 +3,38 @@ import React,{useContext} from 'react';
 //Context
 import { CartContext } from '../../context/CartContextProvider';
 
+//styles
+import styles from './Cart.module.css';
+
 //helper
 import { shorten } from '../../helper/function';
+
+//Icon
+import deleteIcon from '../../assets/icons/trash.svg'
 
 const Cart = (props) => {
     const {image ,title, price , quantity} = props.data;
     const { dispatch } = useContext(CartContext);
 
     return (
-        <div>
-            <img src={image} alt='product' />
-            <div>
-                <h2>{shorten(title)}</h2>
-                <h2>{price} $</h2>
+        <div className={styles.container}>
+            <img src={image} className={styles.productImage} alt='product' />
+            <div className={styles.data}>
+                <h3>{shorten(title)}</h3>
+                <p>{price} $</p>
             </div>
-            <p>{quantity}</p>
-            <button onClick={() => dispatch({type: "DECREASE" , payload : props.data})}> - </button>
-            <button onClick={() => dispatch({type: "INCREASE" , payload : props.data})}> + </button>
+            <div>
+                <span className={styles.quantity}>{quantity}</span>
+            </div>
+            <div className={styles.buttonContainer}>
+                {
+                     quantity > 1 ? 
+                     <button onClick = {() => dispatch ({type : "DECREASE" , payload : props.data})}> - </button> :
+                     <button onClick={() => dispatch({type : "REMOVE_ITEM" , payload : props.data})}><img src={deleteIcon} alt="delete" style={{width: "20px"}} /></button>
+                }
+                <button onClick={() => dispatch({type :"INCREASE" , payload : props.data})}> + </button>
+                
+            </div>
         </div>
     );
 };
